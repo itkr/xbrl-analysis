1c38d9ccd7fe687492f64f8214d2f34589b5bfb6
Shota Itakura <1531860+itkr@users.noreply.github.com>
2022-06-16 14:35:46 +0900

    クラス化

--- main.py
+++ main.py
@@ -5,7 +5,31 @@ from typing import Optional, Union
 from bs4 import BeautifulSoup
 from edinet_xbrl.edinet_xbrl_parser import EdinetXbrlParser
 
+# xbrl
+#     taxonomy
+#         keys
+#             context
+#                 xbrl_data
+#                     xbrl_data_value
+
 # TODO: edinet_xbrlを使うのをやめる
+
+
+@dataclass
+class Context:
+    context_id: str
+    # identifier: str
+    startDate: Optional[str] = None
+    endDate: Optional[str] = None
+    instant: Optional[str] = None
+
+    def __repr__(self):
+        base = f'{self.__class__.__name__} {self.context_id}'
+        if self.startDate:
+            return f'<{base} start={self.startDate} end={self.endDate}>'
+        elif self.instant:
+            return f'<{base} instant={self.instant}>'
+        return f'<{base}>'
 
 
 @dataclass
@@ -21,16 +45,7 @@ class XbrlData:
         return f'    {self.context_ref} {self.value} {self.unit_ref} {self.decimals}'
 
 
-@dataclass
-class Context:
-    context_id: str
-    # identifier: str
-    startDate: Optional[str]
-    endDate: Optional[str]
-    instant: Optional[str]
-
-
-class Converter:
+class XBRLDataValueConverter:
 
     @classmethod
     def convert_value(cls, taxonomy_name: str, key_name: str, value) -> Union[
@@ -61,7 +76,6 @@ class Converter:
         # タグ無し文字
         return soup.html.body.p.text != value
 
-
     @classmethod
     def is_number(cls, value):
         try:
@@ -70,11 +84,9 @@ class Converter:
         except Exception:
             return False
 
-
     @classmethod
     def is_boolean_text(cls, value):
         return value in ['true', 'false']
-
 
     @classmethod
     def is_date_text(cls, value):
@@ -85,63 +97,72 @@ class Converter:
             return False
 
 
-# イメージ
-class XBRL:
+class Taxonomy:
 
-    def __init__(self):
-        pass
+    taxonomy_dictionary = {
+        'jpcrp': '企業内容等の開示に関する内閣府令',
+        'jppfs': '日本基準の勘定科目',
+        'jpigp': '国際会計基準(IFRS)の勘定科目',
+        'jpdei': '文書定義 - 提出文書のメタデータ(提出日付やタイトルなど)を定義するタクソノミ)',
+    }
 
-    def get_taxonomies(self):
-        return []
-
-    def get_contexts(self):
-        return []
-
-    def get_data_by(self, **kwargs):
+    @classmethod
+    def get_description(cls, taxonomy_name):
+        for key, value in cls.taxonomy_dictionary.items():
+            if taxonomy_name.startswith(key):
+                return value
         return ''
 
-    def to_csv(self):
-        pass
 
-    def to_pandas(self):
-        pass
-
-    def make_balance_sheet(self, context_ref=None):
-        pass
-
-    def make_profit_and_loss_statement(self, context_ref=None):
-        pass
-
-
-def sample():
-    xbrl = XBRL()
-    xbrl.get_data_by(taxonomy='jpdei', context_ref='context')
-
-
-taxonomy_dictionary = {
-    'jpcrp': '企業内容等の開示に関する内閣府令',
-    'jppfs': '日本基準の勘定科目',
-    'jpigp': '国際会計基準(IFRS)の勘定科目',
-    'jpdei': '文書定義 - 提出文書のメタデータ(提出日付やタイトルなど)を定義するタクソノミ)',
-}
-
-
-def get_taxonomy_description(taxonomy_name):
-    for key, value in taxonomy_dictionary.items():
-        if taxonomy_name.startswith(key):
-            return value
-    return ''
-
-
-class Loader:
+class XBRL:
 
     def __init__(self, xbrl_file_path):
         self.xbrl_file_path = xbrl_file_path
+        self.load()
+
+    def load(self):
+        # TODO: 二重管理解消 1
         with open(self.xbrl_file_path, 'r') as f:
             self.soup = BeautifulSoup(f.read(), 'lxml')
+        # TODO: 二重管理解消 2
+        self.edinet_xbrl_object = EdinetXbrlParser().parse_file(self.xbrl_file_path)
+
+    def print_values(self, prefix=None):
+        result = {}
+        for key in self.get_keys_by(prefix):
+            data_list = self.edinet_xbrl_object.get_data_list(
+                key, auto_lower=False)
+            try:
+                taxonomy_name, key_name = key.split(':')
+            except ValueError:
+                # 'html', 'body' が入ってくる
+                continue
+
+            if taxonomy_name not in result.keys():
+                result[taxonomy_name] = []
+
+            print(key)
+            # TODO: これだとkeyの改装を無視してtaxonomyごとにdataを入れてしまっている。これでいいか？
+            result[taxonomy_name] = [XbrlData(
+                context_ref=data.get_context_ref(),
+                decimals=data.get_decimals(),
+                key_name=key_name,
+                taxonomy_name=taxonomy_name,
+                value=XBRLDataValueConverter.convert_value(
+                    taxonomy_name, key_name, data.get_value()),
+                unit_ref=data.get_unit_ref()) for data in data_list]
+            for v in result[taxonomy_name]:
+                print(v)
+
+        return result
+
+    def get_taxonomies(self):
+        # TODO: 実装
+        raise
 
     def get_taxonomy_names(self):
-        return [taxonomy.split(':')[1] for taxonomy in list(self.soup.find('xbrli:xbrl').attrs)]
+        return [taxonomy.split(':')[1]
+                for taxonomy in list(self.soup.find('xbrli:xbrl').attrs)]
 
     def get_contexts(self):
         # <xbrli:context id="Prior4YearDuration">
@@ -169,56 +190,52 @@ class Loader:
                 raise ValueError
         return contexts
 
-    def print_xbrl_values(self, edinet_xbrl_object, prefix=None):
-        keys = edinet_xbrl_object.get_keys()
+    def count_keys_by(self, taxonomy_name=None):
+        return len(list(self.get_keys_by(taxonomy_name)))
+
+    def get_keys_by(self, taxonomy_name=None):
+        prefix = taxonomy_name
+        keys = self.edinet_xbrl_object.get_keys()
         if prefix:
-            keys = list(filter(lambda x: x.startswith(prefix), keys))
-        result = {}
+            return list(filter(lambda x: x.startswith(prefix), keys))
+        return keys
 
-        for key in keys:
-            data_list = edinet_xbrl_object.get_data_list(key, auto_lower=False)
-            try:
-                taxonomy_name, key_name = key.split(':')
-            except ValueError:
-                # 'html', 'body' が入ってくる
-                continue
+    def get_data_by(self, **kwargs):
+        # by taxonomy
+        # by key
+        raise
 
-            if taxonomy_name not in result.keys():
-                result[taxonomy_name] = []
+    def to_csv(self):
+        raise
 
-            print(key)
-            # TODO: これだとkeyの改装を無視してtaxonomyごとにdataを入れてしまっている。これでいいか？
-            result[taxonomy_name] = [XbrlData(
-                context_ref=data.get_context_ref(),
-                decimals=data.get_decimals(),
-                key_name=key_name,
-                taxonomy_name=taxonomy_name,
-                value=Converter.convert_value(taxonomy_name, key_name, data.get_value()),
-                unit_ref=data.get_unit_ref()) for data in data_list]
-            for v in result[taxonomy_name]:
-                print(v)
+    def to_pandas(self):
+        raise
 
-    # return result
-    def load(self):
-        edinet_xbrl_object = EdinetXbrlParser().parse_file(self.xbrl_file_path)
-        keys = edinet_xbrl_object.get_keys()
+    def make_balance_sheet(self, context_ref=None):
+        raise
 
-        # 利用タクソノミ一覧
-        for taxonomy in sorted(self.get_taxonomy_names()):
-            print(taxonomy, get_taxonomy_description(taxonomy))
-            print(f'  {len(list(filter(lambda x: x.startswith(taxonomy), keys)))}')
-
-        # コンテキスト一覧
-        # contexts = self.get_contexts()
-        # for context in contexts:
-            # print(context)
-
-        self.print_xbrl_values(edinet_xbrl_object)
-        # self.print_xbrl_values(edinet_xbrl_object, 'jpdei_cor')
+    def make_profit_and_loss_statement(self, context_ref=None):
+        raise
 
 
 def main():
-    Loader('public.xbrl').load()
+    xbrl = XBRL('public.xbrl')
+
+    # 利用タクソノミ一覧
+    for taxonomy_name in sorted(xbrl.get_taxonomy_names()):
+        print(taxonomy_name, Taxonomy.get_description(taxonomy_name))
+        print(f'  {xbrl.count_keys_by(taxonomy_name)}')
+
+    # コンテキスト一覧
+    contexts = xbrl.get_contexts()
+    for context in contexts:
+        print(context)
+
+    # xbrl.get_data_by(taxonomy='jpdei', context_ref='context')
+
+    # 値の表示
+    # xbrl.print_values()
+    xbrl.print_values('jpdei_cor')
 
 
 if __name__ == '__main__':
